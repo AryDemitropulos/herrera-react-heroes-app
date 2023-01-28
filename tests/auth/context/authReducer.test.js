1271a081a0206730a1a0f0c4e96e6735a793f1cf
Ary <demitropulosary@gmail.com>
2023-01-28 11:26:50 -0300

    Pruebas sobre el authReducer

---
+++ tests/auth/context/authReducer.test.js
@@ -0,0 +1,34 @@
+import { authReducer, types } from '../../../src/auth';
+
+describe('Pruebas sobre authReducer', () => {
+  test('debe de retornar el estado por defecto', () => {
+    const state = authReducer({ logged: false }, {});
+    
+    expect(state).toEqual({ logged: false });
+  });
+
+  test('debe de (login) llamar el login autenticar y establecer el user', () => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'TEST',
+        name: 'TEST_NAME',
+      },
+    };
+
+    const newState = authReducer({ logged: false }, action);
+
+    expect(newState).toEqual({ logged: true, user: action.payload });
+  });
+
+  test('debe de (logout) borrar el name del usuario y logged false', () => {
+    const state = { logged: true, user: { id: 'testID', name: 'testName' } };
+    const action = {
+      type: types.logout,
+    };
+
+    const newState = authReducer(state, action);
+
+    expect(newState).toEqual({ logged: false, user: {} });
+  });
+});
